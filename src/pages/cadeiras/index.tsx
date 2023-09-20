a348cc410e78e656871ac1dd6a8db06140927352
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-09-20 08:54:14 -0300

    start

---
+++ src/pages/cadeiras/index.tsx
@@ -0,0 +1,30 @@
+
+import styles from './Cadeira.module.scss'
+import Logo from 'assets/logo.svg'
+import Buscador from './buscador';
+import React, { useState } from 'react';
+import Filtros from './filtros';
+
+export default function Cardapio(){
+  const [buscar, setBusca] = useState("");
+    return (
+        <main>
+            <nav className={ styles.menu }>
+            <img src={Logo} alt="Logo da loja"/>
+            {/*opcoes de cadeiras*/}
+            </nav>
+            <header className={ styles.header }>
+                <div className= { styles.header__text }>
+                  "A casa da massa gostosa e saborosa e deliciosa!"
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+              <h3 className={styles.cardapio__titulo} >cardapio</h3>
+              <Buscador busca={buscar} setBusca={setBusca}/>
+              <div className={styles.cardapio__filtros}>
+                <Filtros/>
+              </div>
+            </section>
+        </main>
+    );
+}
